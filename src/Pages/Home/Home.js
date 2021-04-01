07268794fe8cee89baaf7cfe772de856c5fd6876
unknown <tarunvravi@gmail.com>
2021-04-01 15:16:32 -0400

    implemented game statistics

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -36,6 +36,39 @@ function Home() {
     });
   });
 
+  //---
+  var dbStatsLocation = db.ref(
+    "/" +
+      user.uid +
+      "/-" +
+      window.location.href.split("opponent-")[1] +
+      "/games"
+  );
+  const dbStatsQuery = dbStatsLocation.orderByChild("timestamp");
+  const [snapshotsStats, loadingStats, errorStats] = useList(dbStatsQuery);
+  var userWins = 0;
+  var opponentWins = 0;
+  var draws = 0;
+
+  if (!loadingStats && snapshotsStats) {
+    snapshotsStats.map((value, index) => {
+      switch (value.val().isUser) {
+        case "user":
+          userWins++;
+          break;
+        case "opponent":
+          opponentWins++;
+          break;
+        case "draw":
+          draws++;
+          break;
+        default:
+          console.log("problem with stats");
+          console.log(value.val());
+      }
+    });
+  }
+
   return (
     <div className="Home">
       <div className="opponentTextDiv">
@@ -46,9 +79,23 @@ function Home() {
       <div className="widgets">
         <SaveGame userName={userName} opponentName={opponent} />
         <div className="gamesWon">
-          <GamesWon isUser name={userName} wins="100" total="150" />
-          <GamesWon name={opponent} wins="50" total="150" />
-          <GamesWon isUser="draw" name="Draws" wins="0" total="150" />
+          <GamesWon
+            isUser
+            name={userName}
+            wins={userWins}
+            total={userWins + opponentWins + draws}
+          />
+          <GamesWon
+            name={opponent}
+            wins={opponentWins}
+            total={userWins + opponentWins + draws}
+          />
+          <GamesWon
+            isUser="draw"
+            name="Draws"
+            wins={draws}
+            total={userWins + opponentWins + draws}
+          />
         </div>
         <GameHistory />
       </div>
